5ab83ca12fd946bca686620ab9885ebe583f7167
Adarsh Raj <adarshraj.ar3000@gmail.com>
2026-05-05 10:45:39 +0530

    Updated API URL for production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3000/chat", {
+      const response = await fetch("https://rag-chatbot-70xu.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question: input }),
